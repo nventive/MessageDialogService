cf86f200c58c512571ee3cd93a94ee974a25b097
Gabriel Chouinard Létourneau <gabriel.chouinard@nventive.com>
2022-09-06 13:30:53 -0500

    refactor: separate extensions with proper names

--- Shared/Extensions/CollectionExtensions.cs
+++ Shared/Extensions/CollectionExtensions.cs
@@ -1,22 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace MessageDialogService
 {
-	public static partial class MessageDialogBuilderDelegateExtensions
+	public static partial class CollectionExtensions
 	{
-		/// <summary>
-		/// Remove null values while enumerating
-		/// </summary>
-		internal static IEnumerable<T> Trim<T>(this IEnumerable<T> items)
-			where T : class
-		{
-			return items.Where(item => item != null);
-		}
-
 		/// <summary>
 		/// Replaces the items in a collection with a new set of items.
 		/// </summary>
@@ -38,33 +28,6 @@ namespace MessageDialogService
 		internal static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
 		{
 			items.ForEach<T>(collection.Add);
-		}
-
-		/// <summary>
-		/// Apply an action for every item of an enumerable
-		/// </summary>
-		/// <remarks>
-		/// This method allows looping on every item of the source without enumerating it
-		/// If enumeration is not a concern, you should avoid using this method if you're doing fuctionnal or declarative programming.
-		/// </remarks>
-		internal static void ForEach<T>(this IEnumerable enumerable, Action<T> action)
-		{
-			var list = enumerable as IList;
-
-			if (list == null)
-			{
-				foreach (var item in list)
-				{
-					action((T)item);
-				}
-			}
-			else
-			{
-				for (int i = 0; i < list.Count; i++)
-				{
-					action((T)list[i]);
-				}
-			}
 		}
 	}
 }
---
+++ Shared/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MessageDialogService
+{
+	public static partial class EnumerableExtensions
+	{
+		/// <summary>
+		/// Remove null values while enumerating
+		/// </summary>
+		internal static IEnumerable<T> Trim<T>(this IEnumerable<T> items)
+			where T : class
+		{
+			return items.Where(item => item != null);
+		}
+
+		/// <summary>
+		/// Apply an action for every item of an enumerable
+		/// </summary>
+		/// <remarks>
+		/// This method allows looping on every item of the source without enumerating it
+		/// If enumeration is not a concern, you should avoid using this method if you're doing fuctionnal or declarative programming.
+		/// </remarks>
+		internal static void ForEach<T>(this IEnumerable enumerable, Action<T> action)
+		{
+			var list = enumerable as IList;
+
+			if (list == null)
+			{
+				foreach (var item in list)
+				{
+					action((T)item);
+				}
+			}
+			else
+			{
+				for (int i = 0; i < list.Count; i++)
+				{
+					action((T)list[i]);
+				}
+			}
+		}
+	}
+}
